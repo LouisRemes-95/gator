eb1935365de8162766253f34339fabda9d7ea3ac
Louis Remes <louisremes@hotmail.be>
2025-08-27 00:05:15 +0200

    Finished config file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/LouisRemes-95/gator
+module gator
 
 go 1.24.3
+
+require github.com/LouisRemes-95/gator v0.0.0-20250826214109-cf3068388e20
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/LouisRemes-95/gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatal("Failed to load config:", err)
+	}
+
+	err = cfg.SetUser("Louis")
+	if err != nil {
+		log.Fatal("Failed to set user:", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatal("Failed to load config:", err)
+	}
+
+	fmt.Print(cfg)
 }
