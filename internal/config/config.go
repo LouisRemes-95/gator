cf3068388e20133d38b205b0ef80a1eb2df6727d
Louis Remes <louisremes@hotmail.be>
2025-08-26 23:41:09 +0200

    Add SetUser method

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,28 +7,65 @@ import (
 	"path/filepath"
 )
 
+const configFileName = ".gatorconfig.json"
+
 type Config struct {
 	DbUrl           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
 }
 
 func Read() (Config, error) {
-	homeDir, err := os.UserHomeDir()
+	filePath, err := getConfigFilePath()
 	if err != nil {
-		return Config{}, fmt.Errorf("failed to get user home dir: %w", err)
+		return Config{}, fmt.Errorf("failed to get config file path: %w", err)
 	}
 
-	filePath := filepath.Join(homeDir, ".gatorconfig.json")
 	data, err := os.ReadFile(filePath)
 	if err != nil {
-		return Config{}, fmt.Errorf("failed to read the config file: %w", err)
+		return Config{}, fmt.Errorf("failed to read config file %q: %w", filePath, err)
 	}
 
 	var config Config
 	err = json.Unmarshal(data, &config)
 	if err != nil {
-		return Config{}, fmt.Errorf("failed to decode the json data: %w", err)
+		return Config{}, fmt.Errorf("failed to decode config file %q: %w", filePath, err)
 	}
 
 	return config, nil
+}
+
+func (c *Config) SetUser(userName string) error {
+	c.CurrentUserName = userName
+
+	err := c.write()
+	if err != nil {
+		return fmt.Errorf("failed to write the config: %w", err)
+	}
+	return nil
+}
+
+func getConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("failed to get user home dir: %w", err)
+	}
+	return filepath.Join(homeDir, configFileName), nil
+}
+
+func (c Config) write() error {
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return fmt.Errorf("failed to get config file path: %w", err)
+	}
+
+	data, err := json.Marshal(c)
+	if err != nil {
+		return fmt.Errorf("failed to marshal the config: %w", err)
+	}
+
+	err = os.WriteFile(filePath, data, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write to the config file: %w", err)
+	}
+	return nil
 }
